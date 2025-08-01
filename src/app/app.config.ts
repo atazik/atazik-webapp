fc9e8bf93687a891e1f520018587007ac6615019
sachabarbet <sacha.barbet@proton.me>
2025-08-01 22:06:10 +0200

    Fix name environment files and folder

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import {provideRouter} from '@angular/router';
 
 import {routes} from './app.routes';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
-import {environment} from '../environnements/environnement';
+import {environment} from '../environments/environment';
 import {getAuth, provideAuth} from '@angular/fire/auth';
 import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
