65686f9fa0aa733ae779ace2fa164ddaf8610e94
sachabarbet <sacha.barbet@proton.me>
2025-08-02 15:31:00 +0200

    Add eslint and prettier dependencies and config; Add lint command to ci

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,14 @@
-import {ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-import {routes} from './app.routes';
-import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
-import {environment} from '../environments/environment';
-import {getAuth, provideAuth} from '@angular/fire/auth';
-import {getFirestore, provideFirestore} from '@angular/fire/firestore';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {providePrimeNG} from 'primeng/config';
+import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
-
 
 export const appConfig: ApplicationConfig = {
 	providers: [
@@ -24,12 +23,12 @@ export const appConfig: ApplicationConfig = {
 			ripple: true,
 			theme: {
 				preset: Aura,
-			}
+			},
 		}),
 
 		// Angular imports
 		provideBrowserGlobalErrorListeners(),
-		provideZoneChangeDetection({eventCoalescing: true}),
-		provideRouter(routes)
-	]
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(routes),
+	],
 };
