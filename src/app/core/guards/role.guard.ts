56119f3604a16cb8496586705db1df3c4b7a1607
sachabarbet <sacha.barbet@proton.me>
2025-08-15 16:21:56 +0200

    Prevent redirect on reload with role guard

--- src/app/core/guards/role.guard.ts
+++ src/app/core/guards/role.guard.ts
@@ -3,10 +3,18 @@ import { inject } from "@angular/core";
 import { RoleService } from "../services/role.service";
 import { isUserRoleEqualOrHigher } from "@shared/utils/user-role.utils";
 import { UserRoleEnum } from "@shared/enums/user-roles.enum";
+import { toObservable } from "@angular/core/rxjs-interop";
+import { filter, map, take } from "rxjs";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const roleGuard: CanActivateFn = (route, state) => {
 	const requiredRole = route.data["role"] as UserRoleEnum;
-	const role = inject(RoleService).role();
-	return isUserRoleEqualOrHigher(requiredRole, role);
+	const roleService = inject(RoleService);
+	// Prevent to quit page with needed role on reloading page even if the user have the right role
+
+	return toObservable(roleService.role).pipe(
+		filter((role): role is UserRoleEnum => role !== null && role !== undefined),
+		take(1),
+		map((role) => isUserRoleEqualOrHigher(requiredRole, role)),
+	);
 };
